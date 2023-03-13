a1f5b82d3a6313eaa30ffc6b086fb831537ecc71
happydez <mega6ban@mail.ru>
2023-03-13 16:31:46 +0300

    completed

--- Various/6/main.cpp
+++ Various/6/main.cpp
@@ -2,14 +2,34 @@
 
 using namespace std;
 
+int Is_base_16(char num)
+{
+    if (num == 'A') return 10;
+    if (num == 'B') return 11;
+    if (num == 'C') return 12;
+    if (num == 'D') return 13;
+    if (num == 'E') return 14;
+    if (num == 'F') return 15;
+
+    return -1;
+}
+
 int to_10(string num, int base)
 {
+    int t;
     int res = 0;
     int id = num.length() - 1;
 
     for (int i = 0; i < num.length(); i++)
     {
-        res += (int)(num[i] - '0') * pow(base, id);
+        if ((t = Is_base_16(num[i])) != -1)
+        {
+            res += t * pow(base, id);
+        }
+        else
+        {
+            res += (int)(num[i] - '0') * pow(base, id);
+        }
         id--;
     }
 
@@ -18,10 +38,10 @@ int to_10(string num, int base)
 
 int main()
 {
-    int num16 = 0x1AFF;
+    std::string num16 = "1AFF";
     cout << "In 16 base: " << num16 << endl;
 
-    int num10 = to_10("1AFF", 16);
+    int num10 = to_10(num16, 16);
     cout << "In 10 base: " << num10 << endl;
 
     std::string num8 = to_n(num10, 8);
