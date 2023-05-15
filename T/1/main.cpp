dea86dc651bfd50f0b9e13ae63195a604728a5a1
happydez <mega6ban@mail.ru>
2023-05-15 04:44:11 +0300

    X/11 completed

--- T/1/main.cpp
+++ T/1/main.cpp
@@ -1,33 +1,10 @@
 #include <iostream>
-#include <ctime>
-#include <cmath>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-int sum(const int* mas, const int n);
-void print(const int* mas, const int n);
-int const n = 5;
-
 int main() {
 
-    int marks[n] = {3, 4, 5, 4, 4};
-    cout << "sum: " << sum(marks, n) << endl;
-    print(marks, n);
-
     return 0;
-}
-
-int sum(const int* mas, const int n) {
-    int s = 0;
-    for (int i = 0; i < n; i++) {
-        s += mas[i];
-    }
-    return s;
-}
-
-void print(const int* mas, const int n) {
-    for (int i = 0; i < n; i++) {
-        cout << mas[i] << " ";
-    }
-    cout << "\n";
 }
